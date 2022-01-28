724d84a537271e7b546f33d32830fa6a7b1afc01
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2022-01-28 19:37:05 +0100

    hitbox

--- Front/js/classJs/GameObject.js
+++ Front/js/classJs/GameObject.js
@@ -1,7 +1,11 @@
+/**
+ * Class that manage ball
+ */
 export default class Ball
 {
     x = 0;
     y = 0;
+    rayon = 20
     vx = 0;
     vy = 0;
     constructor(x,y)
@@ -11,12 +15,31 @@ export default class Ball
         this.vx = 2;
         this.vy = 5;
     }
+    /**
+     * Draw a ball on the canvas
+     * @param {context of canvas} ctx 
+     */
     drawBall(ctx)
     {
         ctx.beginPath();
-        ctx.arc(this.x,this.y,20,0,Math.PI*2,true);
+        ctx.arc(this.x,this.y,this.rayon,0,Math.PI*2,true);
         ctx.closePath();
         ctx.fill();
         
     }
+    hitBox(RectX,RectY,sizeX,sizeY)
+    {
+        if(this.y > RectY && this.y <= RectY+sizeY )
+        {
+            if(this.x > RectX && this.x <= RectX+sizeX)
+            {
+                
+                this.vy = (-this.vy);
+            }
+        }
+    }
+
 }
+/**
+ * Class that manage the player
+ */
--- Front/js/principalLoop.js
+++ Front/js/principalLoop.js
@@ -1,19 +1,24 @@
 import Ball from "./classJs/GameObject.js";
+import Player from "./classJs/Player.js";
 
 
 
-
-
+//Get Element in the document
 let canvas = document.getElementById("sheet");
 let context = canvas.getContext("2d");
-let ball = new Ball(0,0)
+
+let ball = new Ball(0,0);
+let player = new Player(500,400,200,10,10);
 let req;
 
 function draw()
 {
     
     context.clearRect(0,0,canvas.width,canvas.height)
+    player.drawPlayer(context)
+
     ball.drawBall(context);
+
     if(ball.x > canvas.width || ball.x < 0 )
     {
         ball.vx = (-ball.vx)
@@ -24,7 +29,8 @@ function draw()
     }
     ball.x += ball.vx;
     ball.y += ball.vy;
-    console.log(ball.y)
+    
+    ball.hitBox(player.x,player.y,player.sizeX,player.sizeY);
     
     window.requestAnimationFrame(draw)
 
@@ -44,7 +50,23 @@ canvas.addEventListener("mouseout",()=>{
     console.log("mouseout");
     window.cancelAnimationFrame(req);
 })
+function inputPlayer()
+{
+    window.addEventListener("keydown",(event)=>{
+        console.log(event.code)
+        switch (event.code) {
+            case "ArrowLeft":
+                player.movePlayerWithLimit(-1,canvas.width)
+                break;
+            case "ArrowRight":
+                player.movePlayerWithLimit(1,canvas.width);
+                break;
+            default:
+                break;
+        }
+    });
+}
 
 
-
+inputPlayer();
 draw()
