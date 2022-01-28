afca1af95d0c2a823820e6ada5def8065b81f122
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2022-01-29 00:13:14 +0100

    drawBrick

---
+++ Front/js/classJs/brickGameObject.js
@@ -0,0 +1,20 @@
+class Brick
+{
+    x = 0;
+    y = 0;
+    sizeX = 0;
+    sizeY = 0;
+
+    constructor(x,y,sizeX,sizeY)
+    {
+        this.x = x;
+        this.y = y;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+    drawBrick(ctx)
+    {
+        ctx.rect(this.x,this.y,this.sizeX,this.sizeY);
+        ctx.fill();
+    }
+}
--- Front/js/principalLoop.js
+++ Front/js/principalLoop.js
@@ -1,5 +1,6 @@
 import Ball from "./classJs/GameObject.js";
 import Player from "./classJs/Player.js";
+import Brick from "./classJs/brickGameObject"
 
 
 
@@ -9,14 +10,18 @@ let context = canvas.getContext("2d");
 
 let ball = new Ball(0,0);
 let player = new Player(500,400,200,10,10);
+let brickGril = [];
 let req;
 
 function draw()
 {
-    
-    context.clearRect(0,0,canvas.width,canvas.height)
-    player.drawPlayer(context)
+    //erase canvas
+    context.clearRect(0,0,canvas.width,canvas.height);
+    //brick
 
+    // player
+    player.drawPlayer(context);
+    //ball
     ball.drawBall(context);
 
     if(ball.x > canvas.width || ball.x < 0 )
@@ -35,6 +40,24 @@ function draw()
     window.requestAnimationFrame(draw)
 
 }
+function initLineBrick(sizeX,sizeY,step,maxwidth,y)
+{
+    lineBrick = [];
+    nbBrick=maxwidth/(step*2+sizeX);
+    
+    for(i = 0;i<nbBrick;i++)
+    {
+        starPos=(step+sizeX)*i;
+        let brick = new Brick((starPos+step),y,sizeX,sizeY);
+        lineBrick.push(brick);
+        
+    }
+    brickGril.push(lineBrick);
+}
+function drawGrillBrick()
+{
+    
+}
 //Update Animation
 function update()
 {
@@ -50,6 +73,7 @@ canvas.addEventListener("mouseout",()=>{
     console.log("mouseout");
     window.cancelAnimationFrame(req);
 })
+//input player
 function inputPlayer()
 {
     window.addEventListener("keydown",(event)=>{
